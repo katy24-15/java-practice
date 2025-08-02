05c1b8a0b161908b41e390859af3dd0a6e803609
Katyayni Singh <katyaynis51@gmail.com>
2025-08-02 20:00:31 +0530

    Added question

--- conditional-statement/MonthsName.java
+++ conditional-statement/MonthsName.java
@@ -1,3 +1,7 @@
+/**
+Ask the user to enter the number of the month & print the name of the month. For eg - For ‘1’ print ‘January’, ‘2’ print ‘February’ & so on.
+*/
+
 import java.util.Scanner;
 public class MonthsName{
   public static void main(String[] args){
