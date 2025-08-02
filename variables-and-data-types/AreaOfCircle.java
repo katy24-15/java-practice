f89855005b189d4feb99a6d471d3727037d0e636
Katyayni Singh <katyaynis51@gmail.com>
2025-08-02 19:55:36 +0530

    Added question

--- variables-and-data-types/AreaOfCircle.java
+++ variables-and-data-types/AreaOfCircle.java
@@ -1,3 +1,7 @@
+/**
+Make a program that takes the radius of a circle as input, calculates its radius and area and prints it as output to the user.
+*/
+
 import java.util.Scanner;
 public class AreaOfCircle{
     public static void main(String[] args) {
