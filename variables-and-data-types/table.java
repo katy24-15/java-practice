8db049a9d682a74dc3f9acea151b48e1a089c503
Katyayni Singh <katyaynis51@gmail.com>
2025-08-02 10:42:58 +0530

    Update table.java

--- variables-and-data-types/table.java
+++ variables-and-data-types/table.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class main{
+public class table{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter number you want the table of: ");
