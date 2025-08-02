360a4780b210ac0bf93345932f8b36068c3b910b
Katyayni Singh <katyaynis51@gmail.com>
2025-08-02 19:58:23 +0530

    Added

--- conditional-statement/calculator.java
+++ conditional-statement/calculator.java
@@ -1,3 +1,13 @@
+/**
+Make a Calculator. Take 2 numbers (a & b) from the user and an operation as follows : 
+1 : + (Addition) a + b
+2 : - (Subtraction) a - b
+3 : * (Multiplication) a * b
+4 : / (Division) a / b
+5 : % (Modulo or remainder) a % b
+Calculate the result according to the operation given and display it to the user.
+*/
+
 import java.util.Scanner;
 public class calculator{
     public static void main(String[] args){
@@ -36,4 +46,5 @@ public class calculator{
     }
     }
 }
+
 
