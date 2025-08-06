6107ca8fd39be37546ac16999a1fee8960c7c4d6
Katyayni Singh <katyaynis51@gmail.com>
2025-08-06 21:57:15 +0530

    Added question

--- patterns/Inverted_Half_Pyramid_With_Numbers.java
+++ patterns/Inverted_Half_Pyramid_With_Numbers.java
@@ -1,3 +1,11 @@
+/**
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+*/
+
 public class Inverted_Half_Pyramid_With_Numbers {
     public static void main(String[] args) {
         int n = 5;
@@ -9,3 +17,4 @@ public class Inverted_Half_Pyramid_With_Numbers {
         }
     }
 }
+
