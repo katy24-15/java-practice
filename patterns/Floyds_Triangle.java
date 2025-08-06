602641ddd3cd2b2b40874314b38fafbb6e0055a6
Katyayni Singh <katyaynis51@gmail.com>
2025-08-06 22:00:12 +0530

    Added question

--- patterns/Floyds_Triangle.java
+++ patterns/Floyds_Triangle.java
@@ -1,3 +1,11 @@
+/**  
+1
+2  3
+4  5  6
+7  8  9  10
+11 12 13 14 15
+*/
+
 public class Floyds_Triangle {
     public static void main(String[] args) {
         int n = 5;
@@ -11,3 +19,4 @@ public class Floyds_Triangle {
         }
     }
 }
+
