fb22aa3eb1241e7d38b8d257eb6969817dd2d810
Katyayni Singh <katyaynis51@gmail.com>
2025-08-16 17:52:35 +0530

    Create Pascal's_Triangle.java

---
+++ patterns/Pascal's_Triangle.java
@@ -0,0 +1,26 @@
+/**
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
+*/
+
+public class kk {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            // spaces 
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            int num = 1; 
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                // for next value
+                num = num * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
